1aecec06cd64589168a76e33881ff8d08213d2b7
Christopher Pauli <chpa5089@colorado.edu>
2025-08-14 00:34:33 -0600

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,12 @@
+// client/src/api.js
+const API = import.meta.env.VITE_API || "http://localhost:5000";
+export const fetchProducts = async (params = {}) => {
+  const qs = new URLSearchParams(params).toString();
+  const res = await fetch(`${API}/products${qs ? `?${qs}` : ""}`, {
+    credentials: "include",
+  });
+  const data = await res.json();
+  if (!data.ok) throw new Error(data.message || "Failed to load products");
+  return data.products;
+};
 
